ae398b1f7cddc21462912a0d4d0c87a5d4e8761d
Óscar de Arriba <odarriba@gmail.com>
2020-04-21 16:09:20 +0200

    Add prod config

--- config/prod.exs
+++ config/prod.exs
@@ -10,46 +10,16 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :phoenix_heroku_example, PhoenixHerokuExampleWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  http: [port: {:system, "PORT"}, compress: true],
+  url: [scheme: "https", host: System.fetch_env!("HOST"), port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: System.fetch_env!("SECRET_KEY_BASE"),
+  server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
 
-# ## SSL Support
-#
-# To get SSL working, you will need to add the `https` key
-# to the previous section and set your `:url` port to 443:
-#
-#     config :phoenix_heroku_example, PhoenixHerokuExampleWeb.Endpoint,
-#       ...
-#       url: [host: "example.com", port: 443],
-#       https: [
-#         port: 443,
-#         cipher_suite: :strong,
-#         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#         certfile: System.get_env("SOME_APP_SSL_CERT_PATH"),
-#         transport_options: [socket_opts: [:inet6]]
-#       ]
-#
-# The `cipher_suite` is set to `:strong` to support only the
-# latest and more secure SSL ciphers. This means old browsers
-# and clients may not be supported. You can set it to
-# `:compatible` for wider support.
-#
-# `:keyfile` and `:certfile` expect an absolute path to the key
-# and cert in disk or a relative path inside priv, for example
-# "priv/ssl/server.key". For all supported SSL configuration
-# options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
-#
-# We also recommend setting `force_ssl` in your endpoint, ensuring
-# no data is ever sent via http, always redirecting to https:
-#
-#     config :phoenix_heroku_example, PhoenixHerokuExampleWeb.Endpoint,
-#       force_ssl: [hsts: true]
-#
-# Check `Plug.SSL` for all available options in `force_ssl`.
-
-# Finally import the config/prod.secret.exs which loads secrets
-# and configuration from environment variables.
-import_config "prod.secret.exs"
+config :phoenix_heroku_example, PhoenixHerokuExample.Repo,
+  ssl: true,
+  url: System.fetch_env!("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
